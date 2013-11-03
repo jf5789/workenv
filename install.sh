43592ec0c15b80bd3bf4ea7f3c7b0a55ac4ab5b7
wellbye <wellbye@163.com>
2013-11-03 23:37:28 +0800

    fix

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
+dir=~/git-repo/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="profile inputrc vimrc vim tmux.conf gitconfig gitignore"    # list of files/folders to symlink in homedir
 
@@ -19,12 +19,12 @@ echo "done"
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
-cd $dir
+cd "$dir"
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-	echo "Moving any existing dotfiles from ~ to $olddir"
+	echo "Moving $file from ~ to $olddir"
 	mv ~/.$file ~/dotfiles_old/
 	echo "Creating symlink to $file in home directory."
 	ln -s $dir/$file ~/.$file
